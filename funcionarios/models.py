6431c0607220dd1ef0f8a10022acdee0c0a3f452
Lzuchi <lucas.zuchii@gmail.com>
2023-05-11 18:04:45 -0300

    First commit

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 from departamentos.models import Departamento
 from empresas.models import Empresa
 
@@ -7,8 +9,11 @@ class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     departamentos = models.ManyToManyField(Departamento)
-    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
+    empresa = models.ForeignKey(
+        Empresa, on_delete=models.PROTECT, null=True, blank=True) #permite em branco
 
+    def get_absolute_url(self):
+        return reverse('list_funcionarios')
     def __str__(self):
         return self.nome
 
