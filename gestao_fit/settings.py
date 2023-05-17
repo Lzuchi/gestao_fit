44229c33d78c5308496e3240394f5ce22e3ac1c7
Lzuchi <lucas.zuchii@gmail.com>
2023-05-16 21:20:25 -0300

    ultimo

--- gestao_fit/settings.py
+++ gestao_fit/settings.py
@@ -96,10 +96,10 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 LOGIN_REDIRECT_URL = 'home'
